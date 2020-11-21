5257aba97845cda9959f8255638900a22dee3566
funbiscuit <kokurinsv@gmail.com>
2020-11-21 17:49:10 +0300

    Update printFig
    Now its easier to provide different options and custom size

--- printFig.m
+++ printFig.m
@@ -1,63 +1,68 @@
-function print_png(filename, size)
-% possible sizes:
-% norm - normal size for one picture 11.5x8 - default
-% wide - fills A4 page horizontally, height 8
-% col2 - size of picture that allows two pictures in row (8x8)
-% ?x? - direct size specification. For example 12x5 or 8.5X4.7
+function printFig(filename, varargin)
+% possible options:
+% size - either [w h] vector or string name. One of the following:
+%        normal - normal size for one picture 11.5x8 - default
+%        wide - fills A4 page horizontally (16.5), height 8
+%        col2 - size of picture that allows two pictures in row (8.2x7.8)
+%        screen - will be saved "as is" but without blank space
+%
+% dpi  - dpi to use when saving picture (300 by default)
 
-sizeProvided = true;
-if ~exist('size','var') || isempty(size)
-    size='norm';
-    sizeProvided = false;
-end
 
+parser = inputParser;
+addOptional(parser,'size','normal',@(x) ~isempty(convertSize(x)));
+addOptional(parser,'dpi',300,@(x) isreal(x)&&isscalar(x)&&(x>1));
+parse(parser,varargin{:});
+sz=convertSize(parser.Results.size);
+dpi=round(parser.Results.dpi);
+
+% create directory if needed
 ind=find(filename=='/',1,'last');
-
 if(length(ind)==1)
     dirname=filename(1:(ind-1));
-    
     if(exist(dirname, 'dir')~=7)
         mkdir(dirname);
     end
 end
 
-% size by default
-newSize = [11.5 8];
-sizeCalculated = false;
-switch size
-    case 'wide'
-        newSize = [16.5 8];
-        sizeCalculated = true;
-    case 'col2'
-        newSize = [8.2 7.8];
-        sizeCalculated = true;
+% save previous values of figure and axes positions
+fig = gcf; ax = gca;
+figPos = fig.Position; axPos = ax.Position;
+if sz(1)~=0
+    % sets figure size in that way so it will be printed to size sz in cm
+    setFigureSize(sz)
 end
 
-% calc size if it was provided manually
-ind = find(size=='x',1);
-if isempty(ind)
-    ind = find(size=='X',1);
+print('-dpng',sprintf('-r%d',dpi),filename)
+
+%restore figure ans axes positions
+ax.Position = axPos;
+if sz(1)~=0
+    fig.Position = figPos;
 end
-if isempty(ind)
-    ind = find(size=='õ',1);%cyrillic
-end
-if isempty(ind)
-    ind = find(size=='Õ',1);%cyrillic
-end
-if ~isempty(ind) && ind>1 && ind<length(size)
-    w = str2double(size(1:ind-1));
-    h = str2double(size(ind+1:end));
-    if ~isnan(w) && ~isnan(h)
-        newSize = [w h];
-        sizeCalculated = true;
+
+function sz=convertSize(sizeArg)
+
+if isnumeric(sizeArg) && length(sizeArg)==2
+    sz=[sizeArg(1) sizeArg(2)];
+elseif ischar(sizeArg)
+    switch sizeArg
+        case 'wide'
+            sz = [16.5 8];
+        case 'col2'
+            sz = [8.2 7.8];
+        case 'normal'
+            sz = [11.5 8];
+        case 'screen'
+            sz = [0 0];
+        otherwise
+            error('Supported values: normal, wide, col2.');
     end
+else
+    error('Size should be either [w h] or string.');
 end
 
-if sizeProvided && ~sizeCalculated
-    fprintf('Bad size provided ''%s'', using default size (%0.1fx%0.1f).\n', size, newSize);
-end
-
-
+function setFigureSize(sz)
 %obtain screen dpi
 set(0,'units','pixels')
 Pix_SS = get(0,'screensize');
@@ -69,11 +74,10 @@ cm2px = Res(end)/2.54;
 
 fig = gcf;
 ax = gca;
-PrevFigPos = fig.Position;
-PrevPos = ax.Position;
 
-fig.Position = [PrevFigPos(1) PrevFigPos(2) ...
-    newSize(1)*cm2px newSize(2)*cm2px];
+figPos = fig.Position;
+
+fig.Position = [figPos(1) figPos(2) sz(1)*cm2px sz(2)*cm2px];
 outerpos = ax.OuterPosition;
 ti = ax.TightInset;
 left = outerpos(1) + ti(1);
@@ -82,8 +86,3 @@ ax_width = outerpos(3) - ti(1) - ti(3);
 ax_height = outerpos(4) - ti(2) - ti(4);
 ax.Position = [left bottom ax_width ax_height];
 
-print('-dpng','-r300',filename)
-
-%restore figure size
-ax.Position = PrevPos;
-fig.Position = PrevFigPos;
